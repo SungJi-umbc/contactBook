1b063f1db84987d3e75f52afd2219477157c7f05
Yumi <amandayee1024@gmail.com>
2023-05-17 22:36:44 -0400

    commit

--- src/main/java/com/contactbook/ContactBookController.java
+++ src/main/java/com/contactbook/ContactBookController.java
@@ -1,13 +1,13 @@
 package com.contactbook;
 
-        import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Scene;
-        import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-        import java.io.IOException;
+import java.io.IOException;
 
-public class ContactBookModel extends Application {
+public class ContactBookController extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ContactBookModel.class.getResource("contactBook-view.fxml"));
--- src/main/java/com/contactbook/ContactBookModel.java
+++ src/main/java/com/contactbook/ContactBookModel.java
@@ -1,15 +1,10 @@
 package com.contactbook;
 
-import javafx.application.Application;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 
-public class ContactBookController
+public class ContactBookModel
 {
     @FXML
     private Label welcomeText;
