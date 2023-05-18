9cbbc60b3ac7bb82c681bcf3ae987b1cb78c096b
Sung Ji <sji2@umbc.edu>
2023-05-17 22:54:19 -0400

    Inside `Contacts`, added "phoneCell", "address", "emailPrimary", and "emailSecondary"

--- src/main/java/com/contactbook/Contacts.java
+++ src/main/java/com/contactbook/Contacts.java
@@ -5,22 +5,28 @@ import javafx.beans.property.SimpleStringProperty;
 public class Contacts {
     private final SimpleStringProperty firstName = new SimpleStringProperty("");
     private final SimpleStringProperty lastName = new SimpleStringProperty("");
-    private final SimpleStringProperty email = new SimpleStringProperty("");
+
+    private final SimpleStringProperty phoneCell = new SimpleStringProperty("");
+    private final SimpleStringProperty address = new SimpleStringProperty("");
+    private final SimpleStringProperty emailPrimary = new SimpleStringProperty("");
+    private final SimpleStringProperty emailSecondary = new SimpleStringProperty("");
 
     public Contacts () {
-        this("", "", "");
+        this("", "", "", "","", "");
     }
 
-    public Contacts (String firstName, String lastName, String email) {
+    public Contacts (String firstName, String lastName, String phoneCell, String address, String emailPrimary, String emailSecondary) {
         setFirstName(firstName);
         setLastName(lastName);
-        setEmail(email);
+        setphoneCell(phoneCell);
+        setAddress(address);
+        setEmailPrm(emailPrimary);
+        setEmailScnd(emailSecondary);
     }
 
     public String getFirstName() {
         return firstName.get();
     }
-
     public void setFirstName(String fName) {
         firstName.set(fName);
     }
@@ -28,17 +34,36 @@ public class Contacts {
     public String getLastName() {
         return lastName.get();
     }
-
     public void setLastName(String fName) {
         lastName.set(fName);
     }
 
-    public String getEmail() {
-        return email.get();
+    public String getphoneCell() {
+        return phoneCell.get();
+    }
+    public void setphoneCell(String fName) {
+        phoneCell.set(fName);
     }
 
-    public void setEmail(String fName) {
-        email.set(fName);
+    public String getAddress() {
+        return address.get();
+    }
+    public void setAddress(String fName) {
+        address.set(fName);
+    }
+
+    public void getEmailPrm(String fName) {
+        emailPrimary.set(fName);
+    }
+    public void setEmailPrm(String fName) {
+        emailPrimary.set(fName);
+    }
+
+    public String getEmailScnd() {
+        return emailSecondary.get();
+    }
+    public void setEmailScnd(String fName) {
+        emailSecondary.set(fName);
     }
 }
 
