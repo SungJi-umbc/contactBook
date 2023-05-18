72b9e765227c65f0c4ce2e06da639466347ca12f
Sung Ji <sji2@umbc.edu>
2023-05-17 21:07:42 -0400

    commit to test1

--- src/main/java/com/example/contactbook/ContactBookController.java
+++ src/main/java/com/example/contactbook/ContactBookController.java
@@ -1,14 +1,25 @@
 package com.example.contactbook;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
 
 public class ContactBookController {
-    @FXML
-    private Label welcomeText;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("IS413 SP23 ContactBook");
+        Pane myPane = (Pane) FXMLLoader.load(getClass().getResource("fxml_tableview.fxml"));
+        Scene myScene = new Scene(myPane);
+        primaryStage.setScene(myScene);
+        primaryStage.show();
+    }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public static void main(String[] args) {
+        launch(args);
     }
 }
