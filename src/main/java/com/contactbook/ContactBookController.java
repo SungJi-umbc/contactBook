b56d1ae09df3e587d8f0aee5034ab8bbafb10a43
Yumi <amandayee1024@gmail.com>
2023-05-17 22:35:27 -0400

    commit

--- src/main/java/com/contactbook/ContactBookController.java
+++ src/main/java/com/contactbook/ContactBookController.java
@@ -1,16 +1,24 @@
 package com.contactbook;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
 
-public class ContactBookController
-{
-    @FXML
-    private Label welcomeText;
+        import java.io.IOException;
 
-    @FXML
-    protected void onHelloButtonClick()
+public class ContactBookModel extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ContactBookModel.class.getResource("contactBook-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 420, 340);
+        stage.setTitle("IS413 SP23 Contact Book Editor");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args)
     {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        launch(args);
     }
 }
--- src/main/java/com/contactbook/ContactBookModel.java
+++ src/main/java/com/contactbook/ContactBookModel.java
@@ -1,24 +1,22 @@
 package com.contactbook;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class ContactBookModel extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ContactBookModel.class.getResource("contactBook-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 420, 340);
-        stage.setTitle("IS413 SP23 Contact Book Editor");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class ContactBookController
+{
+    @FXML
+    private Label welcomeText;
 
-    public static void main(String[] args)
+    @FXML
+    protected void onHelloButtonClick()
     {
-        launch(args);
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
